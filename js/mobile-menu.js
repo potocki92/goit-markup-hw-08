14ac738993b495cc1d2067fe0f09b7f2ceaf9ff0
potocki92 <mateusz.potocki92@gmail.com>
2022-11-08 12:17:46 +0100

    added comments

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,10 +1,17 @@
 (() => {
+  // open the mobile menu
   const openMenu = document.querySelector(".mobile-open");
+  // close the mobile menu
   const closeMenu = document.querySelector(".mobile-close");
+  // to add / remove class 'is-hidden'
   const mobileMenu = document.querySelector(".mobile");
 
+  // click and open mobile menu
   openMenu.addEventListener("click", toggleModal);
+  // click and close mobile menu
   closeMenu.addEventListener("click", toggleModal);
+
+  // function to add 'is-hidden' class to mobile
   function toggleModal() {
    mobileMenu.classList.toggle("is-hidden")
   }
